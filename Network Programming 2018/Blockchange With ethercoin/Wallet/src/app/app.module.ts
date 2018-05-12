89513f60235d56f2b921401110ee220bb89dd77c
kasemsanm <kasemsanm58@email.nu.ac.th>
2018-05-12 14:41:49 +0700

    Fix Error: No component factory found for WalletPage.

--- Network Programming 2018/Blockchange With ethercoin/Wallet/src/app/app.module.ts
+++ Network Programming 2018/Blockchange With ethercoin/Wallet/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SingupPage } from '../pages/singup/singup';
 import { ContractsPage } from '../pages/contracts/contracts';
 import { TradePage } from '../pages/trade/trade';
 import { SettingPage } from '../pages/setting/setting';
+import { WalletPage } from '../pages/wallet/wallet';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { SettingPage } from '../pages/setting/setting';
     SingupPage,
     ContractsPage,
     TradePage,
-    SettingPage
+    SettingPage,
+    WalletPage
   ],
   imports: [
     BrowserModule,
@@ -41,7 +43,8 @@ import { SettingPage } from '../pages/setting/setting';
     SingupPage,
     ContractsPage,
     TradePage,
-    SettingPage
+    SettingPage,
+    WalletPage
   ],
   providers: [
     StatusBar,
